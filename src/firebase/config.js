52e0f2b922936ee0bd7b14f7fd960ea73a2571ab
ngoquangkhoanqkey <ngoquangkhoanqk@gmail.com>
2023-06-20 16:13:48 +0700

    fix conflig

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,56 +1,51 @@
 // Import the functions you need from the SDKs you need
-import {
-    initializeApp
-} from "firebase/app";
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
 
 import {
-    getAnalytics
-} from "firebase/analytics";
-
-import {
-    getAuth,
-    signInWithPopup,
-    signOut,
-    onAuthStateChanged,
-    FacebookAuthProvider,
-    GoogleAuthProvider,
-    getAdditionalUserInfo,
-    // connectAuthEmulator,
+  getAuth,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  getAdditionalUserInfo,
+  // connectAuthEmulator,
 } from "firebase/auth";
 
 import {
-    getFirestore,
-    collection,
-    doc,
-    setDoc,
-    addDoc,
-    getDoc,
-    getDocs,
-    updateDoc,
-    deleteDoc,
-    onSnapshot,
-    serverTimestamp,
-    // connectFirestoreEmulator,
-    query, where, orderBy, limit,
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  onSnapshot,
+  serverTimestamp,
+  // connectFirestoreEmulator,
+  query,
+  where,
+  orderBy,
+  limit,
 } from "firebase/firestore";
-
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCaQXqrLQoG07MHixkncQYEGksxPfmCjkc",
-    authDomain: "chat-appv2-c1c3b.firebaseapp.com",
-    projectId: "chat-appv2-c1c3b",
-    storageBucket: "chat-appv2-c1c3b.appspot.com",
-    messagingSenderId: "977072387934",
-    appId: "1:977072387934:web:109ddedd37df9b0cb541cf",
-    measurementId: "G-Z4ZHNQG0LN"
-  };
-
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 
 // Setup:
 const auth = getAuth();
@@ -58,27 +53,33 @@ const db = getFirestore();
 const fb_provider = new FacebookAuthProvider();
 const gg_provider = new GoogleAuthProvider();
 
-
 // Firebase Emulator:
 // connectAuthEmulator(auth, "http://localhost:9099");
 // connectFirestoreEmulator(db, 'localhost', 8080);
 
-
 export {
-    analytics,
-    db,
-    auth,
-    signInWithPopup,
-    signOut,
-    onAuthStateChanged,
-    fb_provider, gg_provider,
-    FacebookAuthProvider,
-    getAdditionalUserInfo,
-    collection,
-    doc,
-    setDoc,
-    addDoc, getDoc, getDocs, updateDoc, deleteDoc,
-    onSnapshot,
-    serverTimestamp,
-    query, where, orderBy, limit,
+  analytics,
+  db,
+  auth,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  fb_provider,
+  gg_provider,
+  FacebookAuthProvider,
+  getAdditionalUserInfo,
+  collection,
+  doc,
+  setDoc,
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  onSnapshot,
+  serverTimestamp,
+  query,
+  where,
+  orderBy,
+  limit,
 };
